ea97a1aa697e38a46ae3118d831bd7064913eeaa
YakkAttakk <wungemach@gmail.com>
2026-05-26 11:00:24 -0400

    Test with Flask dev server on

--- app.py
+++ app.py
@@ -444,4 +444,5 @@ except Exception as e:
     raise
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
